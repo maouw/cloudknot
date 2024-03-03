37feebd6568f2880408bccff66cf3524a5c45167
Altan Orhon <altan@uw.edu>
2024-03-02 20:31:25 -0800

    WIP

--- cloudknot/commands/configure.py
+++ cloudknot/commands/configure.py
@@ -2,7 +2,9 @@ import docker
 import logging
 import os
 import subprocess
-from awscli.customizations.configure.configure import InteractivePrompter
+import configparser
+import base64
+# from awscli.customizations.configure.configure import InteractivePrompter
 
 from .base import Base
 from ..aws import (
@@ -13,11 +15,30 @@ from ..aws import (
     set_profile,
     set_region,
     set_ecr_repo,
+    list_profiles,
+    clients
 )
 from ..config import add_resource
 
 module_logger = logging.getLogger(__name__)
 is_windows = os.name == "nt"
+
+
+class InteractivePrompter(object):
+    @staticmethod
+    def mask_value(current_value):
+        return None if current_value is None else "*" * 16 + current_value[-4:]
+    
+    def get_value(self, current_value, config_name, prompt_text=""):
+        if config_name in ("aws_access_key_id", "aws_secret_access_key"):
+            current_value = __class__.mask_value(current_value)
+        response = input("%s [%s]: " % (prompt_text, current_value))
+        if not response:
+            # If the user hits enter, we return a value of None
+            # instead of an empty string.  That way we can determine
+            # whether or not a value has changed.
+            response = None
+        return response
 
 
 def pull_and_push_base_images(region, profile, ecr_repo):
@@ -45,6 +66,19 @@ def pull_and_push_base_images(region, profile, ecr_repo):
         ]
     else:
         cmd = ["aws", "ecr", "get-login", "--no-include-email", "--region", region]
+
+    ecr_client = clients["ecr"]
+    response = ecr_client.get_authorization_token()
+
+    username, password = (
+        base64.b64decode(response["authorizationData"][0]["authorizationToken"])
+        .decode()
+        .split(":")
+    )
+
+    registry = response["authorizationData"][0]["proxyEndpoint"]
+
+    cmd = f"docker login -u {username} -p {password} {registry}"
 
     # Refresh the aws ecr login credentials
     login_cmd = subprocess.check_output(cmd, shell=is_windows)
@@ -84,10 +118,66 @@ class Configure(Base):
             "please follow the prompts to start using cloudknot.\n"
         )
 
-        subprocess.call("aws configure".split(" "), shell=is_windows)
+        # If you want to add new values to prompt, update this list here.
+
+        profile = get_profile(fallback=os.environ.get("AWS_PROFILE", "default"))
+        profiles = list_profiles()
+
+        credentials = configparser.ConfigParser()
+        credentials.read(profiles.credentials_file)
+        values_to_prompt = [
+            # (logical_name, config_name, prompt_text)
+            ("aws_access_key_id", "AWS Access Key ID"),
+            ("aws_secret_access_key", "AWS Secret Access Key"),
+        ]
+
+        for config_name, prompt_text in values_to_prompt:
+            prompter = InteractivePrompter()
+            new_value = prompter.get_value(
+                current_value=credentials.get(profile, config_name, fallback=None),
+                config_name=config_name,
+                prompt_text=prompt_text,
+            )
+
+            if new_value is not None:
+                if profile not in credentials:
+                    credentials.add_section(profile)
+                credentials.set(profile, config_name, new_value)
+                with open(profiles.credentials_file, "w") as f:
+                    credentials.write(f)
+
+        config = configparser.ConfigParser()
+        config.read(profiles.aws_config_file)
+        if profile not in config:
+            config.add_section(profile)
+            with open(profiles.aws_config_file, "w") as f:
+                config.write(f)
+                config.read(profiles.aws_config_file)
+        values_to_prompt = [
+            # (logical_name, config_name, prompt_text)
+            ("region", "Default region name"),
+            ("output", "Default output format"),
+        ]
+
+        for config_name, prompt_text in values_to_prompt:
+            prompter = InteractivePrompter()
+            new_value = prompter.get_value(
+                current_value=config.get(profile, config_name, fallback=None),
+                config_name=config_name,
+                prompt_text=prompt_text,
+            )
+
+            if new_value is not None:
+                if profile not in config:
+                    config.add_section(profile)
+                config.set(profile, config_name, new_value)
+                with open(profiles.aws_config_file, "w") as f:
+                    config.write(f)
+
+        # subprocess.call("aws configure".split(" "), shell=is_windows)
 
         print(
-            "\n`aws configure` complete. Resuming configuration with "
+            "\naws configuration complete. Resuming configuration with "
             "`cloudknot configure`\n"
         )
 
